d5e23976682ed839868fbf0bc6052bddbd54ce42
Kosuke Takahashi <rhcp.kdog@gmail.com>
2023-03-15 00:13:19 +0900

    Modified.

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { getSortedPostsData } from "@/lib/posts";
 import Head from "next/head";
 import Link from "next/link";
 import utilStyles from "../styles/utils.module.css";
+import Date from "@/components/date";
 
 interface Props {
   allPostsData: {
@@ -28,15 +29,17 @@ export default function Home({ allPostsData }: Props) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
+          {allPostsData.map(({ id, date, title }) => {
+            return (
+              <li className={utilStyles.listItem} key={id}>
+                <Link href={`/posts/${id}`}>{title}</Link>
+                <br />
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </Layout>
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,6 +1,8 @@
+import Date from "@/components/date";
 import Layout from "@/components/layout";
 import { getAllPostds, getPostData } from "@/lib/posts";
 import Head from "next/head";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({
   postData,
@@ -17,12 +19,13 @@ export default function Post({
       <Head>
         <title>{postData.title}</title>
       </Head>
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
