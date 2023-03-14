9af458c2425c758dd1ca01f561829e4e334a4cd7
Kosuke Takahashi <rhcp.kdog@gmail.com>
2023-03-14 23:53:56 +0900

    Added packages form markdown parse.

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -49,20 +51,26 @@ export function getAllPostds() {
   });
 }
 
-export function getPostData(id: string) {
+export async function getPostData(id: string) {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
 
   // 投稿のメタデータ部分を解析するために gray-matter を使う
   const matterResult = matter(fileContents);
 
+  // マークダウンを HTML 文字列に変換するために remark を使う
+  const processedContent = await remark()
+    .use(html)
+    .process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
   // データを id と組み合わせる
   return {
     id,
+    contentHtml,
     ...matterResult.data,
   } as {
     id: string;
-    title: string;
-    date: string;
+    [key: string]: string;
   };
 }
